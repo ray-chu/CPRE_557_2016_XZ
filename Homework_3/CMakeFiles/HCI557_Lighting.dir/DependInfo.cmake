9ad7374a461650d135bbcd62703908e979231848
Ray Chu <zhuxian1987@gmail.com>
2016-10-06 20:11:54 -0500

    upload Homework_3

---
+++ Homework_3/CMakeFiles/HCI557_Lighting.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Raye/Project/ME557/CPRE_557_2016_XZ/Homework_3/GLSphere.cpp" "/Users/Raye/Project/ME557/CPRE_557_2016_XZ/Homework_3/CMakeFiles/HCI557_Lighting.dir/GLSphere.cpp.o"
+  "/Users/Raye/Project/ME557/CPRE_557_2016_XZ/gl_common/CoordSystem.cpp" "/Users/Raye/Project/ME557/CPRE_557_2016_XZ/Homework_3/CMakeFiles/HCI557_Lighting.dir/Users/Raye/Project/ME557/CPRE_557_2016_XZ/gl_common/CoordSystem.cpp.o"
+  "/Users/Raye/Project/ME557/CPRE_557_2016_XZ/gl_common/GLAppearance.cpp" "/Users/Raye/Project/ME557/CPRE_557_2016_XZ/Homework_3/CMakeFiles/HCI557_Lighting.dir/Users/Raye/Project/ME557/CPRE_557_2016_XZ/gl_common/GLAppearance.cpp.o"
+  "/Users/Raye/Project/ME557/CPRE_557_2016_XZ/gl_common/GLObject.cpp" "/Users/Raye/Project/ME557/CPRE_557_2016_XZ/Homework_3/CMakeFiles/HCI557_Lighting.dir/Users/Raye/Project/ME557/CPRE_557_2016_XZ/gl_common/GLObject.cpp.o"
+  "/Users/Raye/Project/ME557/CPRE_557_2016_XZ/gl_common/HCI557Common.cpp" "/Users/Raye/Project/ME557/CPRE_557_2016_XZ/Homework_3/CMakeFiles/HCI557_Lighting.dir/Users/Raye/Project/ME557/CPRE_557_2016_XZ/gl_common/HCI557Common.cpp.o"
+  "/Users/Raye/Project/ME557/CPRE_557_2016_XZ/gl_common/Shaders.cpp" "/Users/Raye/Project/ME557/CPRE_557_2016_XZ/Homework_3/CMakeFiles/HCI557_Lighting.dir/Users/Raye/Project/ME557/CPRE_557_2016_XZ/gl_common/Shaders.cpp.o"
+  "/Users/Raye/Project/ME557/CPRE_557_2016_XZ/gl_common/camera.cpp" "/Users/Raye/Project/ME557/CPRE_557_2016_XZ/Homework_3/CMakeFiles/HCI557_Lighting.dir/Users/Raye/Project/ME557/CPRE_557_2016_XZ/gl_common/camera.cpp.o"
+  "/Users/Raye/Project/ME557/CPRE_557_2016_XZ/gl_common/controls.cpp" "/Users/Raye/Project/ME557/CPRE_557_2016_XZ/Homework_3/CMakeFiles/HCI557_Lighting.dir/Users/Raye/Project/ME557/CPRE_557_2016_XZ/gl_common/controls.cpp.o"
+  "/Users/Raye/Project/ME557/CPRE_557_2016_XZ/Homework_3/main_lighting.cpp" "/Users/Raye/Project/ME557/CPRE_557_2016_XZ/Homework_3/CMakeFiles/HCI557_Lighting.dir/main_lighting.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/System/Library/Frameworks/OpenGL.framework"
+  "../gl_common"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ Homework_3/CMakeFiles/HCI557_Lighting.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HCI557_Lighting.dir/main_lighting.cpp.o"
+  "CMakeFiles/HCI557_Lighting.dir/Users/Raye/Project/ME557/CPRE_557_2016_XZ/gl_common/HCI557Common.cpp.o"
+  "CMakeFiles/HCI557_Lighting.dir/Users/Raye/Project/ME557/CPRE_557_2016_XZ/gl_common/controls.cpp.o"
+  "CMakeFiles/HCI557_Lighting.dir/Users/Raye/Project/ME557/CPRE_557_2016_XZ/gl_common/CoordSystem.cpp.o"
+  "CMakeFiles/HCI557_Lighting.dir/Users/Raye/Project/ME557/CPRE_557_2016_XZ/gl_common/GLObject.cpp.o"
+  "CMakeFiles/HCI557_Lighting.dir/GLSphere.cpp.o"
+  "CMakeFiles/HCI557_Lighting.dir/Users/Raye/Project/ME557/CPRE_557_2016_XZ/gl_common/Shaders.cpp.o"
+  "CMakeFiles/HCI557_Lighting.dir/Users/Raye/Project/ME557/CPRE_557_2016_XZ/gl_common/GLAppearance.cpp.o"
+  "CMakeFiles/HCI557_Lighting.dir/Users/Raye/Project/ME557/CPRE_557_2016_XZ/gl_common/camera.cpp.o"
+  "HCI557_Lighting.pdb"
+  "HCI557_Lighting"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HCI557_Lighting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
